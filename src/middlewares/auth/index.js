eb75375493f82c7a9c054bb1392bbf1a7b191515
farish-jamal <farishjamal98@gmail.com>
2026-01-21 00:25:07 +0530

    refactor: remove debug log for token in isAdmin middleware

--- src/middlewares/auth/index.js
+++ src/middlewares/auth/index.js
@@ -6,8 +6,6 @@ const isAdmin = async (req, res, next) => {
    try {
       const token = req.headers.authorization?.split(' ')[1];
 
-      console.log(token, '>>>> token');
-
       if (!token) {
          return res
             .status(401)
